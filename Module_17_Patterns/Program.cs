fbeb0fea91f02914cc237f3923d2c10f31810af1
Khalmatov <large000@yandex.ru>
2022-01-23 18:25:18 +0300

    ShowAbstractFactory - демонстрация патерна AbstractFactory

--- Module_17_Patterns/Program.cs
+++ Module_17_Patterns/Program.cs
@@ -6,13 +6,25 @@ namespace Module_17_Patterns
     {
         static void Main(string[] args)
         {
-            Application app = new Application();
+            //    Application app = new Application();
 
-            app.Launch("10.30.60.80");
-            Console.WriteLine(app.DbConnection.Configuration);
+            //    app.Launch("10.30.60.80");
+            //    Console.WriteLine(app.DbConnection.Configuration);
 
-            app.DbConnection = DbConnection.GetConnectionInstance("10.30.60.81");
-            Console.WriteLine(app.DbConnection.Configuration);
+            //    app.DbConnection = DbConnection.GetConnectionInstance("10.30.60.81");
+            //    Console.WriteLine(app.DbConnection.Configuration);
+            ShowAbstractFactory();
+        }
+
+        static void ShowAbstractFactory()
+        {
+            Monster orc = new Monster(new OrcFactory());
+            orc.Hit();
+            orc.Move();
+
+            Monster dragon = new Monster(new DragonFactory());
+            dragon.Hit();
+            dragon.Move();
         }
     }
 }
